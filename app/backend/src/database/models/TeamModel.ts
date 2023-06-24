e6e05f10ab826a7ccc267489267e6ed0a1b6c19a
rosamenezes <g.rosamenezes1@gmail.com>
2023-06-24 12:38:29 -0300

    Lint Errors

--- app/backend/src/database/models/TeamModel.ts
+++ app/backend/src/database/models/TeamModel.ts
@@ -1,47 +1,46 @@
 import {
-    DataTypes,
-    Model,
-    InferAttributes,
-    InferCreationAttributes,
-    CreationOptional,
-  } from 'sequelize';
-  import db from '.';
-  // import OtherModel from './OtherModel';
-  
-  class TeamModel extends Model<InferAttributes<TeamModel>,
-  InferCreationAttributes<TeamModel>> {
-    declare id: CreationOptional<number>;
-    declare teamName: CreationOptional<string>;
-  }
-  
-  TeamModel.init({
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    teamName: {
-      field: 'team_name',
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  }, {
-    sequelize: db,
-    modelName: 'teams',
-    timestamps: false,
-  });
-  
-  /**
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class TeamModel extends Model<InferAttributes<TeamModel>,
+InferCreationAttributes<TeamModel>> {
+  declare id: CreationOptional<number>;
+  declare teamName: CreationOptional<string>;
+}
+
+TeamModel.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    field: 'team_name',
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+/**
     * `Workaround` para aplicar as associations em TS:
     * Associations 1:N devem ficar em uma das instâncias de modelo
     * */
-  
-  // OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
-  // OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
-  
-  // Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
-  // Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
-  
-  export default TeamModel;
-  
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default TeamModel;
